aa7575aed0713fe4f9b2799d45bc7639977a96d9
NPhuongAnh966 <npanh966@gmail.com>
2026-03-05 11:18:26 +0700

    Add files via upload

---
+++ C4_b5/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace c4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.a = new System.Windows.Forms.Label();
+			this.nudA = new System.Windows.Forms.NumericUpDown();
+			this.b = new System.Windows.Forms.Label();
+			this.nudB = new System.Windows.Forms.NumericUpDown();
+			this.c = new System.Windows.Forms.Label();
+			this.nudC = new System.Windows.Forms.NumericUpDown();
+			this.grbNgiem = new System.Windows.Forms.GroupBox();
+			this.txtX2 = new System.Windows.Forms.TextBox();
+			this.txtX1 = new System.Windows.Forms.TextBox();
+			this.lblx2 = new System.Windows.Forms.Label();
+			this.lblx1 = new System.Windows.Forms.Label();
+			this.txtKetQua = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.btnGiai = new System.Windows.Forms.Button();
+			this.btnThoat = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudC)).BeginInit();
+			this.grbNgiem.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// a
+			// 
+			this.a.AutoSize = true;
+			this.a.Location = new System.Drawing.Point(70, 34);
+			this.a.Name = "a";
+			this.a.Size = new System.Drawing.Size(24, 25);
+			this.a.TabIndex = 0;
+			this.a.Text = "a";
+			// 
+			// nudA
+			// 
+			this.nudA.Location = new System.Drawing.Point(117, 32);
+			this.nudA.Name = "nudA";
+			this.nudA.Size = new System.Drawing.Size(120, 31);
+			this.nudA.TabIndex = 1;
+			// 
+			// b
+			// 
+			this.b.AutoSize = true;
+			this.b.Location = new System.Drawing.Point(286, 34);
+			this.b.Name = "b";
+			this.b.Size = new System.Drawing.Size(24, 25);
+			this.b.TabIndex = 2;
+			this.b.Text = "b";
+			// 
+			// nudB
+			// 
+			this.nudB.Location = new System.Drawing.Point(337, 32);
+			this.nudB.Name = "nudB";
+			this.nudB.Size = new System.Drawing.Size(120, 31);
+			this.nudB.TabIndex = 3;
+			// 
+			// c
+			// 
+			this.c.AutoSize = true;
+			this.c.Location = new System.Drawing.Point(520, 32);
+			this.c.Name = "c";
+			this.c.Size = new System.Drawing.Size(23, 25);
+			this.c.TabIndex = 4;
+			this.c.Text = "c";
+			// 
+			// nudC
+			// 
+			this.nudC.Location = new System.Drawing.Point(565, 32);
+			this.nudC.Name = "nudC";
+			this.nudC.Size = new System.Drawing.Size(120, 31);
+			this.nudC.TabIndex = 5;
+			// 
+			// grbNgiem
+			// 
+			this.grbNgiem.Controls.Add(this.txtX2);
+			this.grbNgiem.Controls.Add(this.txtX1);
+			this.grbNgiem.Controls.Add(this.lblx2);
+			this.grbNgiem.Controls.Add(this.lblx1);
+			this.grbNgiem.Location = new System.Drawing.Point(406, 110);
+			this.grbNgiem.Name = "grbNgiem";
+			this.grbNgiem.Size = new System.Drawing.Size(364, 228);
+			this.grbNgiem.TabIndex = 6;
+			this.grbNgiem.TabStop = false;
+			this.grbNgiem.Text = "Nghiệm";
+			// 
+			// txtX2
+			// 
+			this.txtX2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.txtX2.Location = new System.Drawing.Point(104, 126);
+			this.txtX2.Name = "txtX2";
+			this.txtX2.ReadOnly = true;
+			this.txtX2.Size = new System.Drawing.Size(226, 31);
+			this.txtX2.TabIndex = 3;
+			// 
+			// txtX1
+			// 
+			this.txtX1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.txtX1.Location = new System.Drawing.Point(104, 67);
+			this.txtX1.Name = "txtX1";
+			this.txtX1.ReadOnly = true;
+			this.txtX1.Size = new System.Drawing.Size(226, 31);
+			this.txtX1.TabIndex = 2;
+			// 
+			// lblx2
+			// 
+			this.lblx2.AutoSize = true;
+			this.lblx2.Location = new System.Drawing.Point(31, 133);
+			this.lblx2.Name = "lblx2";
+			this.lblx2.Size = new System.Drawing.Size(53, 25);
+			this.lblx2.TabIndex = 1;
+			this.lblx2.Text = "x2 =";
+			// 
+			// lblx1
+			// 
+			this.lblx1.AutoSize = true;
+			this.lblx1.Location = new System.Drawing.Point(31, 74);
+			this.lblx1.Name = "lblx1";
+			this.lblx1.Size = new System.Drawing.Size(53, 25);
+			this.lblx1.TabIndex = 0;
+			this.lblx1.Text = "x1 =";
+			// 
+			// txtKetQua
+			// 
+			this.txtKetQua.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.txtKetQua.Location = new System.Drawing.Point(39, 123);
+			this.txtKetQua.Multiline = true;
+			this.txtKetQua.Name = "txtKetQua";
+			this.txtKetQua.ReadOnly = true;
+			this.txtKetQua.Size = new System.Drawing.Size(333, 215);
+			this.txtKetQua.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(50, 197);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(0, 31);
+			this.textBox2.TabIndex = 7;
+			// 
+			// btnGiai
+			// 
+			this.btnGiai.Location = new System.Drawing.Point(156, 381);
+			this.btnGiai.Name = "btnGiai";
+			this.btnGiai.Size = new System.Drawing.Size(154, 45);
+			this.btnGiai.TabIndex = 8;
+			this.btnGiai.Text = "Giải PTBH";
+			this.btnGiai.UseVisualStyleBackColor = true;
+			this.btnGiai.Click += new System.EventHandler(this.btnGiai_Click);
+			// 
+			// btnThoat
+			// 
+			this.btnThoat.Location = new System.Drawing.Point(406, 381);
+			this.btnThoat.Name = "btnThoat";
+			this.btnThoat.Size = new System.Drawing.Size(154, 45);
+			this.btnThoat.TabIndex = 9;
+			this.btnThoat.Text = "Thoát";
+			this.btnThoat.UseVisualStyleBackColor = true;
+			this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnThoat);
+			this.Controls.Add(this.btnGiai);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.txtKetQua);
+			this.Controls.Add(this.grbNgiem);
+			this.Controls.Add(this.nudC);
+			this.Controls.Add(this.c);
+			this.Controls.Add(this.nudB);
+			this.Controls.Add(this.b);
+			this.Controls.Add(this.nudA);
+			this.Controls.Add(this.a);
+			this.Name = "Form1";
+			this.Text = "Giai phuong trinh bac 2";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudC)).EndInit();
+			this.grbNgiem.ResumeLayout(false);
+			this.grbNgiem.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.Label c;
+        private System.Windows.Forms.NumericUpDown nudC;
+        private System.Windows.Forms.GroupBox grbNgiem;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.Label lblx2;
+        private System.Windows.Forms.Label lblx1;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnGiai;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
